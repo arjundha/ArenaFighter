5962bd0df64277785858d92bd29c1cfcdfc79001
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-09 14:56:44 -0800

    Added level up methods, and changed the wizard character class to a merchant (it makes more sense)

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ to choose their character's:
 
 - Name
 - Race (*Human, dwarf or elf*)
-- Class (*Warrior, rouge or mage*)
+- Class (*Warrior, rouge or merchant*)
 
 As an avid fan of video games and D&D, I chose to create a game that would implement features 
 from D&D to create fun gameplay for anyone to enjoy.
@@ -20,9 +20,14 @@ being implemented. The stories implemented in each stage can be seen as follows:
 User Story | Phase Completed 
 --- | --- 
 As a user, I want to be able to add weapons to my inventory | --- 
+As a user, I want items to modify my characters current stats | ---
 As a user, I want to be able to buy potions to restore my health| ---
 As a user, I want to be able to create a character | ---
+As a user, I want to be able to choose a race for my character | ---
+As a user, I want to be able choose a class for my character | ---
 As a user, I want to be able to choose whether to fight or shop | ---
 As a user, I want to be able to shop | ---
 As a user, I want to be able to fight | ---
 As a user, I want to be able to fight different enemies | -- 
+As a user, I want to be able to level up after winning a fight | ---
+As a user, I want to my stats to change when I level up | ---
--- src/main/player/Character.java
+++ src/main/player/Character.java
@@ -10,8 +10,8 @@ import java.util.List;
  */
 public class Character {
     private String name;  // the name of a character
-    private String race;  // the race of a character (Human, dwarf, elf etc.)
-    private String className;  // the class of a character (Warrior, rogue, wizard etc.)
+    private String race;  // the race of a character (human, dwarf, elf etc.)
+    private String className;  // the class of a character (warrior, rogue, merchant etc.)
     private int level;  // represents the current level of a character
     private List<Integer> hitpoints;  // HP is a list, where the first index is current, and second is max
     private int strength; // The strength of a character
@@ -23,6 +23,14 @@ public class Character {
 
     private static final int STARTING_LEVEL = 1;  // These fields determine what level a character starts at
     public static final int STARTING_GOLD = 50;  // and what amount of gold they should have at the beginning of a game
+
+    public static final int WARRIOR_HEALTH_INCREASE = 10;  // The amount of HP a warrior increases on level up or train
+    public static final int ROGUE_HEALTH_INCREASE = 5;  // The amount of HP a rogue increases on level up
+    public static final int MERCHANT_HEALTH_INCREASE = 1;  // Merchants gain little HP when levelling up or training
+
+    public static final int FAVOURABLE_STAT_INCREASE = 3; //
+    public static final int UNFAVOURABLE_STAT_INCREASE = 1; //
+
 
     /*
     REQUIRES: all integer values must be > 0
@@ -40,7 +48,7 @@ public class Character {
         this.speed = spd;
         this.inventory = new Inventory();
 
-        if (className.equals("wizard")) {  // in this game, wizards always start with more gold due to aLcHeMy
+        if (className.equals("merchant")) {  // in this game, merchants always start with more gold
             this.gold = STARTING_GOLD * 2;
         } else {
             this.gold = STARTING_GOLD;
@@ -106,6 +114,50 @@ public class Character {
     public int getGold() {
         return this.gold;
     }
+
+    // MODIFIES: this
+    // EFFECTS: increase level by one and gain gold
+    //          Merchants earn twice as much gold as other classes
+    public void levelUp() {
+        this.level += 1;
+        // Merchants gain twice as much gold on level up
+        if (this.className.equals("merchant")) {
+            this.gold += STARTING_GOLD * 2;
+        } else {
+            this.gold += STARTING_GOLD;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: randomly increase a players stats based on which class they are
+    //          Warriors gain the most HP, strength and endurance
+    //          Rogues gain the most dexterity and speed
+    //          Merchants gain the least of all stats, but gain more gold on level up to compensate
+    public void increaseStats(String className) {
+        if (className.equals("warrior")) {
+            this.hitpoints.set(1, (this.hitpoints.get(1) + WARRIOR_HEALTH_INCREASE));  // Warrior hp increases
+            this.strength += FAVOURABLE_STAT_INCREASE;  // Warriors gain more strength and endurance
+            this.endurance += FAVOURABLE_STAT_INCREASE;
+            this.dexterity += UNFAVOURABLE_STAT_INCREASE;
+            this.speed += UNFAVOURABLE_STAT_INCREASE;
+
+        } else if (className.equals("rogue")) {
+            this.hitpoints.set(1, (this.hitpoints.get(1) + ROGUE_HEALTH_INCREASE));  // ROGUE hp increases
+            this.strength += UNFAVOURABLE_STAT_INCREASE;
+            this.endurance += UNFAVOURABLE_STAT_INCREASE;
+            this.dexterity += FAVOURABLE_STAT_INCREASE;  // Rogues gain more dex and speed
+            this.speed += FAVOURABLE_STAT_INCREASE;
+
+        } else {
+            this.hitpoints.set(1, (this.hitpoints.get(1) + MERCHANT_HEALTH_INCREASE));  // Warrior hp increases
+            this.strength += UNFAVOURABLE_STAT_INCREASE;  // Merchants don't gain much from stat increases, but
+            this.endurance += UNFAVOURABLE_STAT_INCREASE; // they gain a lot of gold on level up
+            this.dexterity += UNFAVOURABLE_STAT_INCREASE;
+            this.speed += UNFAVOURABLE_STAT_INCREASE;
+        }
+    }
+
+
 }
 
 
