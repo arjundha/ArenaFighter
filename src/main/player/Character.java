a2023071128ba9d4890942c820b7aaf519930b5b
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-09 12:30:22 -0800

    Created all getter files

--- src/main/player/Character.java
+++ src/main/player/Character.java
@@ -13,7 +13,7 @@ public class Character {
     private String race;  // the race of a character (Human, dwarf, elf etc.)
     private String className;  // the class of a character (Warrior, rogue, wizard etc.)
     private int level;  // represents the current level of a character
-    private List<Integer> hitpoints;
+    private List<Integer> hitpoints;  // HP is a list, where the first index is current, and second is max
     private int strength; // The strength of a character
     private int endurance;  // the endurance of a character
     private int dexterity;  // the dexterity of a character
@@ -26,7 +26,7 @@ public class Character {
 
     /*
     REQUIRES: all integer values must be > 0
-    EFFECTS:
+    EFFECTS: Construct a well-formed character
      */
     public Character(String name, String race, String className, int hp, int str, int end, int dex, int spd) {
         this.name = name;
@@ -39,6 +39,7 @@ public class Character {
         this.dexterity = dex;
         this.speed = spd;
         this.inventory = new Inventory();
+
         if (className.equals("wizard")) {  // in this game, wizards always start with more gold due to aLcHeMy
             this.gold = STARTING_GOLD * 2;
         } else {
@@ -46,4 +47,53 @@ public class Character {
         }
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public String getRace() {
+        return this.race;
+    }
+
+    public String getClassName() {
+        return this.className;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getCurrentHealth() {
+        return this.hitpoints.get(0);
+    }
+
+    public int getMaxHealth() {
+        return this.hitpoints.get(1);
+    }
+
+    public int getStrength() {
+        return this.strength + this.inventory.getTotalStrength();
+    }
+
+    public int getEndurance() {
+        return this.endurance + this.inventory.getTotalEndurance();
+    }
+
+    public int getDexterity() {
+        return this.dexterity + this.inventory.getTotalDexterity();
+    }
+
+    public int getSpeed() {
+        return this.speed + this.inventory.getTotalSpeed();
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    public int getGold() {
+        return this.gold;
+    }
 }
+
+
