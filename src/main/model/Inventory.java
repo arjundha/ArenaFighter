fd6e3e4b7cd0e4dcb53590ff8aaf59a3852e98f6
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-08 18:06:50 -0800

    Created an addEquipment() method to the Inventory class

--- src/main/model/Inventory.java
+++ src/main/model/Inventory.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 // Represents all the equipment currently owned by a player (their inventory of equipment)
 public class Inventory {
-    private List<Equipment> items;
+    private List<Equipment> equipment;
 
     // EFFECTS: constructs an empty inventory
     public Inventory() {
-        this.items = new ArrayList<>();
+        this.equipment = new ArrayList<>();
+    }
+
+    // REQUIRES: worth > 0
+    // MODIFIES: this
+    // EFFECTS: adds a new equipment to the inventory of equipment
+    public void addEquipment(String itemName, int str, int end, int dex, int spe, int worth) {
+        Equipment equipment = new Equipment(itemName, str, end, dex, spe, worth);
+        this.equipment.add(equipment);
     }
 
 
