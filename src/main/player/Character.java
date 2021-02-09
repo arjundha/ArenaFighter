ac6665ad24b526e5979f71a9e91bdaa93aff7f23
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-09 12:16:04 -0800

    Characters now have a level

--- src/main/player/Character.java
+++ src/main/player/Character.java
@@ -12,6 +12,7 @@ public class Character {
     private String name;  // the name of a character
     private String race;  // the race of a character (Human, dwarf, elf etc.)
     private String className;  // the class of a character (Warrior, rogue, wizard etc.)
+    private int level;  // represents the current level of a character
     private List<Integer> hitpoints;
     private int strength; // The strength of a character
     private int endurance;  // the endurance of a character
@@ -20,7 +21,8 @@ public class Character {
     private Inventory inventory;  // the inventory of a character
     private int gold;  // the amount of gold a character has
 
-    public static final int STARTING_GOLD = 50;
+    private static final int STARTING_LEVEL = 1;  // These fields determine what level a character starts at
+    public static final int STARTING_GOLD = 50;  // and what amount of gold they should have at the beginning of a game
 
     /*
     REQUIRES: all integer values must be > 0
@@ -30,6 +32,7 @@ public class Character {
         this.name = name;
         this.race = race;
         this.className = className;
+        this.level = STARTING_LEVEL;  // a new character ALWAYS starts at the same level
         this.hitpoints = Arrays.asList(hp, hp); // HP is a list, where the first index is current, and second is max
         this.strength = str;
         this.endurance = end;
