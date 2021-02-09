f79c07d2c85d1b13c6cb6b63f1df89d08333ec96
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-08 19:28:22 -0800

    Created a removeEquipment method, and a getModifierTotals method for Inventory

--- src/main/model/Inventory.java
+++ src/main/model/Inventory.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // Represents all the equipment currently owned by a player (their inventory of equipment)
@@ -15,9 +16,33 @@ public class Inventory {
     // REQUIRES: worth > 0
     // MODIFIES: this
     // EFFECTS: adds a new equipment to the inventory of equipment
-    public void addEquipment(String itemName, int str, int end, int dex, int spe, int worth) {
-        Equipment equipment = new Equipment(itemName, str, end, dex, spe, worth);
+    public void addEquipment(String itemName, int str, int end, int dex, int spd, int worth) {
+        Equipment equipment = new Equipment(itemName, str, end, dex, spd, worth);
         this.equipment.add(equipment);
+    }
+
+    // REQUIRES: 0 <= index < this.equipment.size()
+    // MODIFIES: this
+    // EFFECTS: remove an equipment from an Inventory
+    public void removeEquipment(int index) {
+        this.equipment.remove(index);
+    }
+
+    public List<Integer> getModifierTotals() {
+        int strength = 0;
+        int endurance = 0;
+        int dexterity = 0;
+        int speed = 0;
+//        List<Integer> totals = new ArrayList<>();
+        for (Equipment equipment : this.equipment) {
+            strength += equipment.getStrength();
+            endurance += equipment.getEndurance();
+            dexterity += equipment.getDexterity();
+            speed += equipment.getDexterity();
+        }
+
+        return Arrays.asList(strength, endurance, dexterity, speed);
+
     }
 
 
