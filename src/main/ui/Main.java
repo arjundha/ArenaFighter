4182c703c2bde22ba50b14ed88f256a9125c01ec
Arjun <59514930+arjundha@users.noreply.github.com>
2021-02-12 20:11:46 -0800

    Main has a class-definition

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,9 @@
 package ui;
 
+/**
+ * Class used for starting up a new game. Run this file to begin the game.
+ */
+
 public class Main {
     public static void main(String[] args) {
         new Game();
