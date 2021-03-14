9fb7ca37450a3aa65a89e19e95281cb21241722e
Arjun <59514930+arjundha@users.noreply.github.com>
2021-03-13 22:18:13 -0800

    Created game menu options (currently none are functional) and a header that displays player stats

--- src/main/ui/Game.java
+++ src/main/ui/Game.java
@@ -28,7 +28,7 @@ public class Game extends JFrame {
     private static final int HEIGHT = 700;
     private static final Color BACKGROUND = Color.DARK_GRAY;
     private static final Font TITLE_FONT = new Font("Arial", Font.BOLD, 50);
-    private static final Font NORMAL_FONT = new Font("Arial", Font.PLAIN, 25);
+    private static final Font NORMAL_FONT = new Font("Arial", Font.PLAIN, 21);
 
     private JPanel mainArea;
     private JTextArea mainTextArea;
@@ -59,7 +59,7 @@ public class Game extends JFrame {
 
     private void startScreen() {
         createTitle();
-        generateStartMenu();
+//        generateStartMenu();
     }
 
     private void createTitle() {
@@ -72,6 +72,7 @@ public class Game extends JFrame {
         titleLabel.setFont(TITLE_FONT);
 
         titlePanel.add(titleLabel);
+        generateStartMenu();
         getContentPane().add(titlePanel);
     }
 
@@ -80,9 +81,11 @@ public class Game extends JFrame {
         newGame.setBounds(100, 500, 600, 80);
         newGame.setBackground(BACKGROUND);
 
-        JButton newGameButton = createMainMenuButton("NEW GAME");
+        JButton newGameButton = createMenuButton();
+        newGameButton.setText("NEW GAME");
         newGameButton.addActionListener(new StartNewGameHandler());
-        JButton loadGameButton = createMainMenuButton("LOAD GAME");
+        JButton loadGameButton = createMenuButton();
+        loadGameButton.setText("LOAD GAME");
         loadGameButton.addActionListener(new LoadGameHandler());
 
         newGame.add(newGameButton);
@@ -91,8 +94,8 @@ public class Game extends JFrame {
         getContentPane().add(newGame);
     }
 
-    private JButton createMainMenuButton(String label) {
-        JButton button = new JButton(label);
+    private JButton createMenuButton() {
+        JButton button = new JButton();
         button.setBackground(Color.white);
         button.setForeground(Color.black);
         button.setFont(NORMAL_FONT);
@@ -130,22 +133,124 @@ public class Game extends JFrame {
 
     private void generateGameScreen() {
         mainArea = new JPanel();
-        mainArea.setBounds(100, 100, 600, 250);
+        mainArea.setBounds(50, 50, 700, 300);
         mainArea.setBackground(Color.blue);
+        generateTextArea();
         getContentPane().add(mainArea);
+        generateMenu();
 
-        mainTextArea = new JTextArea();
-        mainTextArea.setBounds(100, 100, 600, 250);
+    }
+
+    private void generateMenu() {
+        JPanel menuArea = new JPanel();
+        menuArea.setBounds(50, 360, 700, 290);
+        menuArea.setLayout(new GridLayout(4, 2));
+        fillMenu(menuArea);
+        getContentPane().add(menuArea);
+    }
+
+    private void fillMenu(JPanel menuArea) {
+        for (int i = 1; i <= 8; i++) {
+            if (i == 1) {
+                menuArea.add(shopButton());
+            } else if (i == 2) {
+                menuArea.add(trainButton());
+            } else if (i == 3) {
+                menuArea.add(healButton());
+            } else if (i == 4) {
+                menuArea.add(fightButton());
+            } else if (i == 5) {
+                menuArea.add(statsButton());
+            } else if (i == 6) {
+                menuArea.add(inventoryButton());
+            } else if (i == 7) {
+                menuArea.add(saveButton());
+            } else {
+                menuArea.add(quitButton());
+            }
+        }
+    }
+
+    private JButton shopButton() {
+        JButton button = createMenuButton();
+        button.setText("1) Shop");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private class ShopHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private JButton trainButton() {
+        JButton button = createMenuButton();
+        button.setText("2) Train");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton healButton() {
+        JButton button = createMenuButton();
+        button.setText("3) Heal");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton fightButton() {
+        JButton button = createMenuButton();
+        button.setText("4) Fight");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton statsButton() {
+        JButton button = createMenuButton();
+        button.setText("5) View stats");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton inventoryButton() {
+        JButton button = createMenuButton();
+        button.setText("6) View inventory");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton saveButton() {
+        JButton button = createMenuButton();
+        button.setText("7) Save");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private JButton quitButton() {
+        JButton button = createMenuButton();
+        button.setText("8) Quit");
+        button.addActionListener(new ShopHandler());
+        return button;
+    }
+
+    private void generateTextArea() {
+        mainTextArea = new JTextArea("What would you like to do?");
+        mainTextArea.setBounds(0, 0, 700, 300);
         mainTextArea.setBackground(Color.BLACK);
         mainTextArea.setForeground(Color.WHITE);
         mainTextArea.setFont(NORMAL_FONT);
         mainTextArea.setLineWrap(true);
+        mainTextArea.setEditable(false);
+//        JScrollPane scroll = new JScrollPane(mainTextArea);
+//        scroll.setBounds(10,60,780,500);
         mainArea.add(mainTextArea);
+
     }
 
     private void generateHeader() {
         JPanel header = new JPanel();
-        header.setBounds(0,0, WIDTH, 50);
+        header.setBounds(0,0, WIDTH - 10, 50);
         header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
         header.setBackground(Color.GRAY);
         fillHeader(header);
---
+++ src/main/ui/buttons/MenuButton.java
@@ -0,0 +1,9 @@
+package ui.buttons;
+
+import javax.swing.*;
+
+public abstract class MenuButton extends JButton {
+    protected JButton button;
+
+
+}
